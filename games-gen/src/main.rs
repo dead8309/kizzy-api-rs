5e9a2d506ebb54f3870311dd55f162fc4ed0eb30
dead8309 <vaibhavmonu8309@gmail.com>
2024-04-27 08:44:39 +0530

    place gnerated gamen in /gen

--- games-gen/src/main.rs
+++ games-gen/src/main.rs
@@ -2,24 +2,29 @@ use futures::future::join_all;
 use models::Game;
 use std::fs;
 
+mod config;
 mod fetch;
 mod models;
-mod xbox;
-mod config;
 mod utils;
+mod xbox;
 
+const OUTPUT_DIR: &str = "../gen/games.json";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let endpoints = config::get_endpoints();
-    let futures = endpoints.iter().map(|endpoint| { fetch::get_games_from_endpoint(endpoint) });
-    let tasks = join_all(futures).await
+    let futures = endpoints
+        .iter()
+        .map(|endpoint| fetch::get_games_from_endpoint(endpoint));
+    let tasks = join_all(futures)
+        .await
         .into_iter()
         .collect::<Vec<Result<Vec<Game>, reqwest::Error>>>();
     let games = tasks
         .iter()
         .flat_map(|task| task.as_ref().unwrap())
         .collect::<Vec<&Game>>();
-    let writer = fs::File::create("games.json")?;
+    let writer = fs::File::create(OUTPUT_DIR)?;
     serde_json::to_writer(writer, &games)?;
+    println!("✅ games.json updated successfully!");
     Ok(())
 }
